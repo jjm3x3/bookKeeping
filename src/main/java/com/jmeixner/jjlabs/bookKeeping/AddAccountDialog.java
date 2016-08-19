5a6e78e71f866fb9ce3f366d62f7d8fc789284d4
Jacob Meixner <jjm3333@gmail.com>
2016-08-18 17:39:50 -0700

    Do some refactoring and protect data against idiot errors

--- src/main/java/com/jmeixner/jjlabs/bookKeeping/AddAccountDialog.java
+++ src/main/java/com/jmeixner/jjlabs/bookKeeping/AddAccountDialog.java
@@ -11,52 +11,70 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class AddAccountDialog extends Dialog<ButtonType> {
-		AddAccountDialog() {
-			DialogPane node = new DialogPane();
-			GridPane gridPane = new GridPane();
-			gridPane.setAlignment(Pos.CENTER);
-			gridPane.setHgap(10);
-			gridPane.setVgap(10);
+	final TextField nameField = new TextField();
+	final TextField startAmountBox = new TextField();
+	
+	
+	AddAccountDialog() {
+		DialogPane node = new DialogPane();
+		GridPane gridPane = new GridPane();
+		gridPane.setAlignment(Pos.CENTER);
+		gridPane.setHgap(10);
+		gridPane.setVgap(10);
 
-			final TextField nameField = new TextField();
-			nameField.setPromptText("Account Name");
-			final TextField startAmountBox = new TextField();
-			startAmountBox.setPromptText("set start amount");
+		nameField.setPromptText("Account Name");
+		startAmountBox.setPromptText("set start amount");
 
-			ButtonType okButtonType =  new ButtonType("OK");
-			node.getButtonTypes().add(okButtonType);
+		ButtonType okButtonType =  new ButtonType("OK");
+		node.getButtonTypes().add(okButtonType);
 
-			Button okButton = (Button) node.lookupButton(okButtonType);
-			okButton.setText("ok");
-			okButton.setOnAction(new EventHandler<ActionEvent>(){
-				@Override
-				public void handle(ActionEvent arg0) {
-					String accountName = nameField.getText();
-					double accountStartAmount = Double.parseDouble(startAmountBox.getText());
-					System.out.println("account name is: " + accountName);
-					System.out.println("with starting amount: " + startAmountBox.getText());
-					AppMain.AddAccount(accountName, accountStartAmount);
-					AddAccountDialog.this.hide();
-				}
-			});
+		Button okButton = (Button) node.lookupButton(okButtonType);
+		okButton.setText("ok");
+		okButton.setOnAction(submitHandler);
 
-			// build ui
-			gridPane.add(nameField, 0, 0);
-			gridPane.add(startAmountBox, 0, 1);
-			gridPane.add(okButton, 0, 2);
-//			gridPane.getChildren().add(text);
-//			gridPane.getChildren().add(okButton);
-			node.setContent(gridPane);
-			setDialogPane(node);
+		// build ui
+		gridPane.add(nameField, 0, 0);
+		gridPane.add(startAmountBox, 0, 1);
+		gridPane.add(okButton, 0, 2);
+		node.setContent(gridPane);
+		setDialogPane(node);
 			
-//			onCloseRequest = new EventHandler(){
-//				
-//			}
+	}
+	
+	private EventHandler<ActionEvent> submitHandler = new EventHandler<ActionEvent>(){
+		@Override
+		public void handle(ActionEvent arg0) {
+			String accountName = nameField.getText();
+			boolean accountNameIsValid = nameIsValid(accountName);
+			double accountStartAmount = 0;
+			boolean startingAmountIsValid = amountIsValid(startAmountBox.getText());
+			if (startingAmountIsValid)
+				accountStartAmount = Double.parseDouble(startAmountBox.getText());
+
+			System.out.println("account name is: " + accountName);
+			System.out.println("with starting amount: " + startAmountBox.getText());
+
+			if (accountNameIsValid && startingAmountIsValid){
+				AppMain.AddAccount(accountName, accountStartAmount);
+			}
 		}
+	};
+
+	private boolean nameIsValid(String possibleName){
+		boolean result = possibleName.length() != 0;
+		if (!result)
+			System.err.println("The account Name must not be empty");
+		return result;
+	}
 		
-//		@Ovrride
-//		public Optional<ButtonType> showAndWait(){
-//			
-//		}
+	private boolean amountIsValid(String possibleDouble){
+		try {
+			Double.parseDouble(possibleDouble);
+			return true;
+		} catch (NumberFormatException e){
+			System.err.println("Unable to parse the startingAmount for this account");
+			return false;
+		} 
+	}
 	
 }
