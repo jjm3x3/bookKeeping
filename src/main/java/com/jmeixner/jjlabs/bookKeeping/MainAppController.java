843cc8bd3efb11c239f8483fd2b4aa22aaa78023
Jacob Meixner <jjm3333@gmail.com>
2016-08-18 20:02:32 -0700

    Finally got the basics down
    This has a view and a controller so that I can abstract out some more
    peices

---
+++ src/main/java/com/jmeixner/jjlabs/bookKeeping/MainAppController.java
@@ -0,0 +1,13 @@
+package com.jmeixner.jjlabs.bookKeeping;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class MainAppController {
+	
+	@FXML protected void createAccount(ActionEvent event){
+		System.out.println("will add an account");
+	}
+
+}
