5a3c5446482ef3c1b23e28fb18d93aeff0756d01
Jacob Meixner <jjm3333@gmail.com>
2016-08-18 20:54:20 -0700

    Finish the last peices now to clean up unsed code

--- src/main/java/com/jmeixner/jjlabs/bookKeeping/MainAppController.java
+++ src/main/java/com/jmeixner/jjlabs/bookKeeping/MainAppController.java
@@ -9,15 +9,16 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
+import models.Account;
 
 public class MainAppController {
-	@FXML private TableView accountList;
+	@FXML private TableView<Account> accountList;
 	
 	@FXML protected void createAccount(ActionEvent event){
 		System.out.println("will add an account");
 		System.out.println("Hi brenna");
 				
-		Dialog alert = new AddAccountDialog();
+		Dialog<ButtonType> alert = new AddAccountDialog();
 		Optional<ButtonType> result = alert.showAndWait();
 		if (result.isPresent() ) {
 			System.out.println("so the account was finalized now with result: " + result.get());
@@ -29,6 +30,21 @@ public class MainAppController {
 			}
 					
 		}
+	}
+
+	@FXML public void addTransaction(ActionEvent event) {
+		int accountNumber = accountList.getSelectionModel().getFocusedIndex();
+		System.out.println(accountNumber + 1);
+		Dialog<ButtonType> addTransactionDialog = new AddTransactionDialog(accountNumber + 1);
+		Optional<ButtonType> result = addTransactionDialog.showAndWait();
+		if (result .isPresent()){
+			try {
+				AppMain.updateAccountTable(accountList);
+			} catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+				
 	}
 
 }
